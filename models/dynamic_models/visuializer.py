9f2680ac58ec1c48f176a1cc041f4bac1dd96909
Digisimon <rokusak@gmail.com>
2024-05-08 11:47:49 +0300

    experimental functionality for the dynamic models

--- models/dynamic_models/visuializer.py
+++ models/dynamic_models/visuializer.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 from typing import List, Optional
+import numpy as np
 
 def show_facility_location(xC: List[float], 
                          yC: List[float], 
@@ -11,14 +12,22 @@ def show_facility_location(xC: List[float],
                          periods: pd.Series, 
                          homes,
                          locations,
+                         vpop: pd.Series,
                          served: Optional[int]=None):
-    plt.plot( xC,yC, 'o' )
+    plt.rcParams["figure.figsize"] = (13,8)
     plt.plot( xF,yF, 's', mfc='none' )
-    [plt.plot( xC[i], yC[i], 'o', color="g") for i in range(len(homes)) for n in range(len(periods)) if Z[n][i]]
+    # Plot households
+    for i in range(len(homes)):
+        if Z[-1][i]:
+            plt.plot(xC[i], yC[i], 'o', color="g", markersize=vpop[i] / 100)  # Size based on population
+        else:
+            plt.plot(xC[i], yC[i], 'o', color="b", markersize=vpop[i] / 100)  # Default marker size
     for j in range(len(locations)):
-      for n in range(len(periods)):
-        if X[n][j] > .5:
+      if X[-1][j] > .5:
           plt.plot( xF[j],yF[j], 's', color='y' )
-        if not served is None:
-            plt.title( '{:.2f}%'.format(served/len(xC)*100) )
+      for n in range(len(periods)-1):
+          if X[n][j] == 0 and X[n+1][j] == 1:
+            plt.text( xF[j],yF[j], str(n+1))
+    if not served is None:
+        plt.title( '{:.2f}%'.format(served/len(xC)*100) )
     plt.show()
