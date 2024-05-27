1b9c7597eb24392eb9ff14d52bf32a04f5507ad9
Digisimon <rokusak@gmail.com>
2024-05-27 18:06:43 +0300

    created solution time function

---
+++ utils/solution_time.py
@@ -0,0 +1,42 @@
+import timeit
+import numpy as np
+import pandas as pd
+from create_data import generate_facility_location_instance
+
+def measure_time(func, distances, homes, locations, periods, per_period, max_distance, open_facilities, vpop, num_passes=10):
+
+    timer = timeit.Timer(lambda: func(distances, homes, locations, periods, per_period, max_distance, open_facilities, vpop))
+    times = timer.repeat(repeat=num_passes, number=1)
+    min_time = np.min(times)
+    avg_time = np.mean(times)
+    return min_time , avg_time
+
+def get_solution_times(functions, params, num_passes=10):
+
+    results = []
+
+    periods_df = pd.DataFrame([1,2,3,4,5,6,7,8,9,10])
+    periods = periods_df[0]
+    per_period = 2
+    max_distance = 3
+
+    for (no_of_households, no_of_facilities) in params:
+        service, customers, facilities  = generate_facility_location_instance(no_of_facilities, no_of_households, 20)
+
+        distances = service.to_numpy()
+        homes = customers
+        locations = facilities
+        open_facilities = facilities["is_built"]
+        vpop = customers["population"]
+
+        for func in functions:
+            min_time, avg_time = measure_time(func, distances, homes, locations, periods, per_period, max_distance, open_facilities, vpop, num_passes)
+            results.append({
+                "Function": func.__name__,
+                "Households": no_of_households,
+                "Facilities": no_of_facilities,
+                "Min Time(s)": min_time,
+                "Avg Time(s)": avg_time
+            })
+
+    return pd.DataFrame(results)
